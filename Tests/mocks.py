e001ca797ad7606d4f583260f9ca92cd7128cade
Chris Hennes <chennes@pioneerlibrarysystem.org>
2025-02-18 15:12:05 -0600

    Add tests

---
+++ Tests/mocks.py
@@ -0,0 +1,87 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+# ***************************************************************************
+# *                                                                         *
+# *   Copyright (c) 2025 The FreeCAD project association AISBL              *
+# *                                                                         *
+# *   This file is part of Telemetry, a FreeCAD addon.                      *
+# *                                                                         *
+# *   Telemetry is free software: you can redistribute it and/or modify it  *
+# *   under the terms of the GNU Lesser General Public License as           *
+# *   published by the Free Software Foundation, either version 2.1 of the  *
+# *   License, or (at your option) any later version.                       *
+# *                                                                         *
+# *   Telemetry is distributed in the hope that it will be useful, but      *
+# *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      *
+# *   Lesser General Public License for more details.                       *
+# *                                                                         *
+# *   You should have received a copy of the GNU Lesser General Public      *
+# *   License along with Telemetry. If not, see                             *
+# *   <https://www.gnu.org/licenses/>.                                      *
+# *                                                                         *
+# ***************************************************************************
+
+"""Mocks for unit-testing the Telemetry Addon outside of FreeCAD, or without interaction with
+FreeCAD (e.g. it won't actually change any user preferences, and doesn't matter that they are
+set to.)"""
+
+from unittest.mock import MagicMock
+
+
+class ParameterMock:
+    """A minimal, fully self-contained mock of FreeCAD's parameter group"""
+
+    def __init__(self, param_dict=None):
+        super().__init__()
+        if param_dict is None:
+            self.param_dict = {}
+        else:
+            self.param_dict = param_dict
+
+    def GetBool(self, key: str, default: bool) -> bool:
+        return self._get(key, default)
+
+    def GetString(self, key: str, default: str) -> str:
+        return self._get(key, default)
+
+    def GetInt(self, key: str, default: int) -> int:
+        return self._get(key, default)
+
+    def SetBool(self, key: str, value: bool):
+        self.param_dict[key] = value
+
+    def SetString(self, key: str, value: str):
+        self.param_dict[key] = value
+
+    def SetInt(self, key: str, value: int):
+        self.param_dict[key] = value
+
+    def _get(self, key: str, default):
+        if key in self.param_dict:
+            return self.param_dict[key]
+        return default
+
+
+class FreeCADMock(MagicMock):
+    """A minimal mock of FreeCAD that allows the creation of parameter groups that can be manipulated
+    the same way real ones can (minus any interaction with FreeCAD's actual parameter system)."""
+
+    def __init__(self):
+        super().__init__()
+        self.param_dicts = {}
+        self.Console = MagicMock()
+
+    def add_parameter_dict(self, group_name: str, param_dict):
+        self.param_dicts[group_name] = param_dict
+
+    def Version(self):
+        return ["1", "2", "3", "", "", "", "main"]
+
+    def ParamGet(self, group_name: str):
+        if group_name in self.param_dicts:
+            return ParameterMock(self.param_dicts[group_name])
+        else:
+            return ParameterMock()
+
+    def getUserAppDataDir(self):
+        return "/Some/Fake/Path/"
---
+++ Tests/test_posthog.py
@@ -0,0 +1,148 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+# ***************************************************************************
+# *                                                                         *
+# *   Copyright (c) 2025 The FreeCAD project association AISBL              *
+# *                                                                         *
+# *   This file is part of Telemetry, a FreeCAD addon.                      *
+# *                                                                         *
+# *   Telemetry is free software: you can redistribute it and/or modify it  *
+# *   under the terms of the GNU Lesser General Public License as           *
+# *   published by the Free Software Foundation, either version 2.1 of the  *
+# *   License, or (at your option) any later version.                       *
+# *                                                                         *
+# *   Telemetry is distributed in the hope that it will be useful, but      *
+# *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      *
+# *   Lesser General Public License for more details.                       *
+# *                                                                         *
+# *   You should have received a copy of the GNU Lesser General Public      *
+# *   License along with Telemetry. If not, see                             *
+# *   <https://www.gnu.org/licenses/>.                                      *
+# *                                                                         *
+# ***************************************************************************
+
+import importlib
+import unittest
+from unittest.mock import patch, MagicMock
+import mocks
+import sys
+
+from Tests.mocks import FreeCADMock
+
+
+class PosthogFCTest(unittest.TestCase):
+
+    def setUp(self):
+        """On setup, ensure the real imports are never executed."""
+        self.fc = FreeCADMock()
+        self.fcg = MagicMock()
+        self.ph = MagicMock()
+
+        self.fc_patch = unittest.mock.patch.dict("sys.modules", {"FreeCAD": self.fc})
+        self.fc_patch.start()
+
+        self.fcg_patch = unittest.mock.patch.dict("sys.modules", {"FreeCADGui": self.fcg})
+        self.fcg_patch.start()
+
+        self.posthog_patch = unittest.mock.patch.dict("sys.modules", {"posthog": self.ph})
+        self.posthog_patch.start()
+
+    def tearDown(self):
+        self.fc_patch.stop()
+        self.fcg_patch.stop()
+        self.posthog_patch.stop()
+        sys.modules.pop("FreeCAD", None)
+        sys.modules.pop("FreeCADGui", None)
+        sys.modules.pop("posthog", None)
+
+    def test_posthog_launch_addon_disabled(self):
+        """Ensure that when disabled no calls to posthog are made"""
+        self.fc.add_parameter_dict(
+            "User parameter:BaseApp/Preferences/Mod/Telemetry", {"Enabled": False}
+        )
+        with patch("posthog.Posthog") as MockPosthog:
+            import PosthogFC
+
+            PosthogFC.posthog_launch()
+            MockPosthog.assert_not_called()
+            instance = MockPosthog.return_value
+            instance.capture.assert_not_called()
+
+    def test_posthog_launch_addon_enabled(self):
+        """Ensure that when enabled, calls to posthog are made"""
+        self.fc.add_parameter_dict(
+            "User parameter:BaseApp/Preferences/Mod/Telemetry", {"Enabled": True}
+        )
+        with patch("posthog.Posthog") as MockPosthog:
+            import PosthogFC
+
+            PosthogFC.posthog_launch()
+            MockPosthog.assert_called()
+            instance = MockPosthog.return_value
+            instance.capture.assert_called()
+
+    def test_posthog_all_info_sent_by_default(self):
+        """Ensure that with defaults used, all info is sent"""
+        with patch("posthog.Posthog") as MockPosthog:
+            import PosthogFC
+
+            PosthogFC.posthog_launch()
+            instance = MockPosthog.return_value
+            args = instance.capture.call_args_list
+            expected_events = [
+                "freecad_version",
+                "freecad_system_info",
+                "freecad_preferences",
+                "freecad_addon_list",
+            ]
+            for arg in args:
+                self.assertIn("event", arg.kwargs)
+                self.assertIn(arg.kwargs["event"], expected_events)
+                expected_events.remove(
+                    arg.kwargs["event"]
+                )  # Mark it as seen by removing it from the list
+            self.assertEqual(
+                len(expected_events), 0
+            )  # If we saw them all, then there is nothing left
+
+    def test_posthog_sysinfo_not_sent_if_disabled(self):
+        self.fc.add_parameter_dict(
+            "User parameter:BaseApp/Preferences/Mod/Telemetry", {"SendSystemInformation": False}
+        )
+        with patch("posthog.Posthog") as MockPosthog:
+            import PosthogFC
+
+            PosthogFC.posthog_launch()
+            instance = MockPosthog.return_value
+            args = instance.capture.call_args_list
+            for arg in args:
+                self.assertIn("event", arg.kwargs)
+                self.assertNotEqual(arg.kwargs["event"], "freecad_system_info")
+
+    def test_posthog_addons_not_sent_if_disabled(self):
+        self.fc.add_parameter_dict(
+            "User parameter:BaseApp/Preferences/Mod/Telemetry", {"SendAddonInformation": False}
+        )
+        with patch("posthog.Posthog") as MockPosthog:
+            import PosthogFC
+
+            PosthogFC.posthog_launch()
+            instance = MockPosthog.return_value
+            args = instance.capture.call_args_list
+            for arg in args:
+                self.assertIn("event", arg.kwargs)
+                self.assertNotEqual(arg.kwargs["event"], "freecad_addon_list")
+
+    def test_posthog_preferences_not_sent_if_disabled(self):
+        self.fc.add_parameter_dict(
+            "User parameter:BaseApp/Preferences/Mod/Telemetry", {"SendPreferences": False}
+        )
+        with patch("posthog.Posthog") as MockPosthog:
+            import PosthogFC
+
+            PosthogFC.posthog_launch()
+            instance = MockPosthog.return_value
+            args = instance.capture.call_args_list
+            for arg in args:
+                self.assertIn("event", arg.kwargs)
+                self.assertNotEqual(arg.kwargs["event"], "freecad_preferences")
