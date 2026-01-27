f7cd8fee4a827a302ddeae83a3d6ff6a6cf1ab7f
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2026-01-27 12:47:12 -0500

    Add missing SPDX annotations

--- Tests/mocks.py
+++ Tests/mocks.py
@@ -1,4 +1,6 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
+# SPDX-FileNotice: Part of the Telemetry addon.
+
 # ***************************************************************************
 # *                                                                         *
 # *   Copyright (c) 2025 The FreeCAD project association AISBL              *
