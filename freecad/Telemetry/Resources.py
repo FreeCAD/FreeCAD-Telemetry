893ccdfe0d6994f6be3ebd0f6ed4d301982b9deb
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2026-01-27 15:52:30 -0500

    Add resource helper

---
+++ freecad/Telemetry/Resources.py
@@ -0,0 +1,42 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+# SPDX-FileNotice: Part of the Telemetry addon.
+
+import freecad.Telemetry as module
+from importlib import resources
+from typing import TypedDict
+
+
+translations = resources.files(module) / 'Resources/Translations'
+interfaces = resources.files(module) / 'Resources/Interfaces'
+icons = resources.files(module) / 'Resources/Icons'
+
+
+class PathsDict ( TypedDict ):
+    Translations : str
+    Icons : str
+
+
+Paths : PathsDict = {
+    'Translations' : str( translations ) ,
+    'Icons' : str( icons )
+}
+
+
+def asInterface ( name : str ):
+
+    file = name + '.ui'
+
+    interface = interfaces / file
+
+    with resources.as_file(interface) as path:
+        return str( path )
+
+
+def asIcon ( name : str ):
+
+    file = name + '.svg'
+
+    icon = icons / file
+
+    with resources.as_file(icon) as path:
+        return str( path )
